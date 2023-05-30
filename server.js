d26ffbf077e7243929413b1da60d0f171a6b76df
zscheel <zachary.scheel1@gmail.com>
2023-05-29 23:05:38 -0500

    edited server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const htmlRoutes = require('./routes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
